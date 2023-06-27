322bf387d56850edef6fe8337327cd0c4bed71e1
Derry Leng <derry.leng@gmail.com>
2023-06-27 19:29:22 +0100

    exercise_05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    return (max * (max + 1) / 2) - ((min - 1) * min / 2);
 };
 
 // Do not edit below this line
