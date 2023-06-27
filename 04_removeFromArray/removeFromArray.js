3bd390bd3336767e2c955f32b0a60aca8ea4a743
Derry Leng <derry.leng@gmail.com>
2023-06-27 19:19:47 +0100

    exercise_04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(origArray, ...args) {
+    let diffArray = origArray.filter(x => !args.includes(x));
+    return diffArray;
 };
 
 // Do not edit below this line
