1fcf1c452586f1fb0377bb9402d879be640a4d66
Derry Leng <derry.leng@gmail.com>
2023-06-27 19:54:12 +0100

    exercise_08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+	return sumArray.reduce((x, y) => x + y, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+	return args.reduce((x, y) => x * y, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0) {
+    return 1;
+  }
+  return x * factorial(x - 1);
 };
 
 // Do not edit below this line
