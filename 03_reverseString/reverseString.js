88b067ff70f97c26b54efaffab55fe7a95d51538
Derry Leng <derry.leng@gmail.com>
2023-06-27 19:19:38 +0100

    exercise_03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(origString) {
+    let revString = "";
+    for (let i = origString.length - 1; i >= 0; i--) {
+        revString += origString[i];
+    }
+    // Or:
+    // let revString = origString.split("").reverse().join("");
+    return revString;
 };
 
 // Do not edit below this line
