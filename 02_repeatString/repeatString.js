04cda0455757b651ccffbd3ebe470c320e45773b
Derry Leng <derry.leng@gmail.com>
2023-06-27 18:36:43 +0100

    exercise_02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) return "ERROR";
+    let fullString = "";
+    for (let i = 0; i < times; i++) {
+        fullString += word;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
