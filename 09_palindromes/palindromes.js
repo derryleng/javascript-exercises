27d5ada25a086ab4a68e7f1fa30fed579e7e3dd8
Derry Leng <derry.leng@gmail.com>
2023-06-27 19:59:10 +0100

    exercise_09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (testString) {
+    let charString = testString.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+    return charString.split("").reverse().join("") == charString;
 };
 
 // Do not edit below this line
